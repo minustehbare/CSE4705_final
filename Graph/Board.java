e2f5145459ec0bf33ff322bfe9b29fcbf24926a1
Steve Northup <Steve.Northup@gmail.com>
2011-04-10 21:20:37 -0400

    Added printBoard and deltaMoves methods to the Board class

--- Graph/Board.java
+++ Graph/Board.java
@@ -29,7 +29,8 @@ public class Board{
   }
   
   //return a list of all neighboring nodes
-  public java.util.List<Node> getNeighbors(int _row, int _col){
+  public java.util.List<Node> getNeighbors(int _row, int _col)
+  {
     java.util.List<Node> _list = new java.util.LinkedList<Node>();
     int _colT;
     int _rowT;
@@ -160,7 +161,8 @@ public class Board{
   }
   
     //mark a space as 'shot'
-    public boolean markShot(int _row,int _col){
+   public boolean markShot(int _row,int _col)
+   {
     if (_board[_row][_col].isMarked){
       System.out.println("ATTEMPTED TO SHOOT A MARKED SPACE: "+_row+" "+_col);
       return false;
@@ -171,7 +173,8 @@ public class Board{
     
     //unmark a shot.  May be usefull for marking where the queens
     //are then removing them when they move.
-    public boolean unmarkShot(int _row,int _col){
+   public boolean unmarkShot(int _row,int _col)
+   {
     if (!_board[_row][_col].isMarked){
       System.out.println("ATTEMPTED TO UNMARK A BLANK SPACE: "+_row+" "+_col);
       return false;
@@ -179,5 +182,41 @@ public class Board{
     _board[_row][_col].isMarked = false;
     return true;
   }
+ 
+  //Grab the magnitudinal change in available moves between one spot and another
+  public int deltaMoves(int _fromRow, int _fromCol, int _toRow, int _toCol)
+  {
+    java.util.List<Node> _fromList = legalMoves(_fromRow, _fromCol);
+    java.util.List<Node> _toList = legalMoves(_toRow, _toCol);
+    return(_toList.size()-_fromList.size());
+  }
   
+  //Can you guess what this method does?
+  public void printBoard()
+  {
+    System.out.print("\n\n -------------------- \n");
+    for(int i=0;i<10;i++)
+    {
+      System.out.print("|");
+      for(int j=0;j<10;j++)
+      {
+        if (_board[i][j].isMarked)System.out.print(" #");
+        else System.out.print(" -");
+      }
+    {System.out.print("|\n");}
+    }
+    System.out.print(" -------------------- \n\n");
+  }
+  
+  public static void main(String[] argv){
+    Board _b = new Board();
+    _b.markShot(0,2);
+    _b.markShot(1,2);
+    _b.markShot(2,2);
+    _b.markShot(1,1);
+    _b.markShot(2,1);
+    _b.markShot(3,0);
+    
+    _b.printBoard();
+  }
 }
