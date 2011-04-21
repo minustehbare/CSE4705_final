4375df601493acc8da7b4b5d0cbb55d09abca37b
Steve Northup <Steve.Northup@gmail.com>
2011-04-21 12:15:14 -0400

    Alpha beta algorithm getting closer to working...

--- EndGame/ABSearch.java
+++ EndGame/ABSearch.java
@@ -1,45 +1,69 @@
 package CSE4705_final.EndGame;
 
-import CSE4705_final.Graph.*;
 import CSE4705_final.State.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class ABSearch{
   
   //entry function.  Start an ABSearch here
-  public Move ABStart(Partition _state, boolean _isBlack){
+  public Move ABStart(Partition _state, int gen, boolean _isBlack){
     //is there a java thing for infinity?
     if(_isBlack)
-      return (MaxValue(_state, new Move(null, null,-9999), new Move(null, null, 9999)));
-    return   (MinValue(_state, new Move(null, null,-9999), new Move(null, null, 9999)));
+      return (MaxValue(_state, gen, new Move(null, null,-9999), new Move(null, null, 9999)));
+    return   (MinValue(_state, gen, new Move(null, null,-9999), new Move(null, null, 9999)));
   }
   
-  private Move MaxValue(Partition _state, Move a, Move b){
+  private Move MaxValue(Partition _state, int gen, Move a, Move b){
+    //Get the index of the white queen
+    Set<Integer> _whiteQueens = _state.getWhiteQueens(gen, true);
+    Iterator<Integer> WQItr = _whiteQueens.iterator();
+    Integer WQindex = WQItr.next();
+
     //if the white queen can't move
-    if(_state._whiteQ.getMoves().size()==0)
+    if(_state.getReachableNodes(WQindex, true).isEmpty())
       return null; //TODO: This will be the best move for black to take with a value that is the number of spaces blackQ can get to.
-    java.util.LinkedList<Vertex> _moves = _state._blackQ.getMoves();
-    if (_moves.size()==0)
+
+    //Get the index of the black queen
+    Set<Integer> _blackQueens = _state.getWhiteQueens(gen, true);
+    Iterator<Integer> BQItr = _whiteQueens.iterator();
+    Integer BQindex = BQItr.next();
+
+    //List of black's possible moves.
+    List<Node> _moves = _state.getReachableNodes(BQindex, true);
+
+    //We can't move!
+    if (_moves.isEmpty())
       return null;
-    java.util.ListIterator<Vertex> _itrM = _moves.listIterator();
-    java.util.ListIterator<Vertex> _itrS;
-    java.util.LinkedList<Vertex> _shots;
+
+    Iterator<Node> _itrM = _moves.iterator();
+    Iterator<Node> _itrS; //Use this to scroll through shots for each move
+    List<Node> _shots;    //Store a list of shots from a given move
     Move _move;
-    Vertex _tempVertex;
+    Node _tempVertex;     //Hold the current move in question
+
+    //This is where the magic happens
     while(_itrM.hasNext()){
+      //Get an iterator oh shots you can make from the first possible move
       _tempVertex = _itrM.next();
-      _shots = _tempVertex.getMoves();
+      _shots = _state.getReachableNodes(_tempVertex.getIndex(), true);
       _itrS = _shots.listIterator();
+
+      //Run through the shots, build new states, and run them recursively
       while(_itrS.hasNext()){
-        //TODO: build a new state, pump it through MinValue, and grab the returned move
-        a = max(a, MinValue(!!NEW STATE!!, a, b));
-        if (b <= a) return a;
+        //TODO: build a new state, pump it through MinValue, and grab the max of the returned move
+        Move _newMove = MinValue(_state, gen, a, b);
+        if(_newMove.getValue() > a.getValue())
+          a = _newMove;
+        if (b.getValue() <= a.getValue()) return a;
       }
     }
     return a;
   }
 
   //TODO: when MaxValue works, paste it here and fix it up real nice for MinValue
-  private Move MinValue(Partition _state, Move a, Move b){
+  private Move MinValue(Partition _state, int gen, Move a, Move b){
     return null;
   }
 }
