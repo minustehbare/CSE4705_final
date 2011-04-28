b7174923df9b63349d5a41119742027c13d6f5d6
Ethan Levine <ethraax@gmail.com>
2011-04-28 16:50:25 -0400

    Rough implementation of PartitionSet.  Still missing many utilities.

---
+++ State/PartitionSet.java
@@ -0,0 +1,175 @@
+package CSE4705_final.State;
+
+import java.util.*;
+
+import CSE4705_final.Client.ClientMove;
+
+/**
+ *
+ * @author Ethan Levine
+ */
+public class PartitionSet {
+    
+    private final List<Partition> _deadParts;
+    private final List<Partition> _whiteOwnedParts;
+    private final List<Partition> _blackOwnedParts;
+    private final List<Partition> _contestedParts;
+    
+    public PartitionSet() {
+        _deadParts = new LinkedList<Partition>();
+        _whiteOwnedParts = new LinkedList<Partition>();
+        _blackOwnedParts = new LinkedList<Partition>();
+        _contestedParts = new LinkedList<Partition>();
+    }
+    
+    public PartitionSet(Partition part) {
+        this();
+        addArbitraryPartition(part);
+    }
+    
+    public PartitionSet(Collection<Partition> parts) {
+        this();
+        for (Partition part : parts) {
+            addArbitraryPartition(part);
+        }
+    }
+    
+    public PartitionSet(List<Partition> deadParts,
+                        List<Partition> whiteOwnedParts,
+                        List<Partition> blackOwnedParts,
+                        List<Partition> contestedParts) {
+        _deadParts = deadParts;
+        _whiteOwnedParts = whiteOwnedParts;
+        _blackOwnedParts = blackOwnedParts;
+        _contestedParts = contestedParts;
+    }
+    
+    public List<Partition> getDeadParts() {
+        return Collections.unmodifiableList(_deadParts);
+    }
+    
+    public List<Partition> getWhiteOwnedParts() {
+        return Collections.unmodifiableList(_whiteOwnedParts);
+    }
+    
+    public List<Partition> getBlackOwnedParts() {
+        return Collections.unmodifiableList(_blackOwnedParts);
+    }
+    
+    public List<Partition> getContestedParts() {
+        return Collections.unmodifiableList(_contestedParts);
+    }
+    
+    private void addArbitraryPartition(Partition part) {
+        switch (part.getPartitionState()) {
+            case DEAD:
+                _deadParts.add(part);
+                break;
+            case BLACK_OWNED:
+                _blackOwnedParts.add(part);
+                break;
+            case WHITE_OWNED:
+                _whiteOwnedParts.add(part);
+                break;
+            case CONTESTED:
+                _contestedParts.add(part);
+                break;
+            default:
+                throw new StateException ("Partition has an invalid state.");
+        }
+    }
+    
+    private PartitionSet forkPartitionSetInternal(Partition active, List<Partition> splitParts) {
+        List<Partition> deadSet = null;
+        List<Partition> whiteOwnedSet = null;
+        List<Partition> blackOwnedSet = null;
+        List<Partition> contestedSet = null;
+        switch (active.getPartitionState()) {
+            case DEAD:
+                deadSet = new LinkedList(_deadParts);
+                if (!deadSet.remove(active)) {
+                    throw new StateException ("Partition \"active\" given to forkPartitionSet() is not registered!");
+                }
+                break;
+            case WHITE_OWNED:
+                whiteOwnedSet = new LinkedList(_whiteOwnedParts);
+                if (!whiteOwnedSet.remove(active)) {
+                    throw new StateException ("Partition \"active\" given to forkPartitionSet() is not registered!");
+                }
+                break;
+            case BLACK_OWNED:
+                blackOwnedSet = new LinkedList(_blackOwnedParts);
+                if (!blackOwnedSet.remove(active)) {
+                    throw new StateException ("Partition \"active\" given to forkPartitionSet() is not registered!");
+                }
+                break;
+            case CONTESTED:
+                contestedSet = new LinkedList(_contestedParts);
+                if (!contestedSet.remove(active)) {
+                    throw new StateException ("Partition \"active\" given to forkPartitionSet() is not registered!");
+                }
+                break;
+            default:
+                throw new StateException ("Partition has an invalid state.");
+        }
+        
+        // Categorize these changes.
+        for (Partition splitPart : splitParts) {
+            switch (splitPart.getPartitionState()) {
+                case DEAD:
+                    if (deadSet == null) {
+                        deadSet = new LinkedList(_deadParts);
+                    }
+                    deadSet.add(splitPart);
+                    break;
+                case WHITE_OWNED:
+                    if (whiteOwnedSet == null) {
+                        whiteOwnedSet = new LinkedList(_whiteOwnedParts);
+                    }
+                    whiteOwnedSet.add(splitPart);
+                    break;
+                case BLACK_OWNED:
+                    if (blackOwnedSet == null) {
+                        blackOwnedSet = new LinkedList(_blackOwnedParts);
+                    }
+                    blackOwnedSet.add(splitPart);
+                    break;
+                case CONTESTED:
+                    if (contestedSet == null) {
+                        contestedSet = new LinkedList(_contestedParts);
+                    }
+                    contestedSet.add(splitPart);
+                    break;
+                default:
+                    throw new StateException ("Partition has an invalid state.");
+            }
+        }
+        
+        // If any partition lists are unchanged, simply pass the reference to
+        // the old one!
+        if (deadSet == null) {
+            deadSet = _deadParts;
+        }
+        if (whiteOwnedSet == null) {
+            whiteOwnedSet = _whiteOwnedParts;
+        }
+        if (blackOwnedSet == null) {
+            blackOwnedSet = _blackOwnedParts;
+        }
+        if (contestedSet == null) {
+            contestedSet = _contestedParts;
+        }
+        
+        // Make and return the new partition set!
+        return new PartitionSet(deadSet, whiteOwnedSet, blackOwnedSet, contestedSet);
+    }
+    
+    public PartitionSet forkPartitionSet(Partition active, int index, NodeState newState) {
+        return forkPartitionSetInternal(active, active.forkNode(index, newState));
+    }
+    
+    public PartitionSet forkPartitionSet(Partition active, ClientMove move,
+            boolean isMovingPlayerBlack) {
+        return forkPartitionSetInternal(active, active.forkMove(move, isMovingPlayerBlack));
+    }
+}
